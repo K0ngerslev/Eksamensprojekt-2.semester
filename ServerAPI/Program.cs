6b2edf5c6cfc3d08ec27c0c3cf7399674760397a
K0ngerslev <andreaskongerslev@yahoo.dk>
2026-05-03 21:40:22 +0200

    v

--- ServerAPI/Program.cs
+++ ServerAPI/Program.cs
@@ -3,7 +3,7 @@ using ServerAPI.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// tilføj mongoDB services nedenunder!!!
 
 var connectionString = builder.Configuration.GetConnectionString("MongoDb")
     ?? throw new InvalidOperationException("Connection string 'MongoDb' is not configured.");
@@ -14,7 +14,6 @@ builder.Services.AddSingleton<IMongoClient>(_ => new MongoClient(connectionStrin
 builder.Services.AddScoped(sp => sp.GetRequiredService<IMongoClient>().GetDatabase(databaseName));
 builder.Services.AddScoped<IAktivitet, AktivitetRepository>();
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
