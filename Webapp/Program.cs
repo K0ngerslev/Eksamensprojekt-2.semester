5f064225e95d5040cfcc618356c34573c2060f2f
K0ngerslev <andreaskongerslev@yahoo.dk>
2026-05-04 12:29:21 +0200

    m

--- Webapp/Program.cs
+++ Webapp/Program.cs
@@ -2,11 +2,14 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Eksamensprojekt_2.semester;
 using Blazored.LocalStorage;
-using blazored.SessionStorage;
+using Blazored.SessionStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddBlazoredSessionStorage();
+
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
