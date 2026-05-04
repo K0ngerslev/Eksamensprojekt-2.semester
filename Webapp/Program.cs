e9ea091bc5a3872aa0e79ec40ae603f8210ff7dd
K0ngerslev <andreaskongerslev@yahoo.dk>
2026-05-04 12:26:15 +0200

    blazor

--- Webapp/Program.cs
+++ Webapp/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Eksamensprojekt_2.semester;
+using Blazored.LocalStorage;
+using blazored.SessionStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
