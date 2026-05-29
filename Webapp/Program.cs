0016de186e9a22519aba043eb0ea4ecbc99e339e
K0ngerslev <andreaskongerslev@yahoo.dk>
2026-05-29 09:02:19 +0200

    muligt fix?

--- Webapp/Program.cs
+++ Webapp/Program.cs
@@ -4,7 +4,6 @@ using Eksamensprojekt_2.semester;
 using Blazored.SessionStorage;
 using WebApp.Service;
 
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
@@ -12,11 +11,12 @@ builder.Services.AddBlazoredSessionStorage();
 builder.Services.AddScoped<LoginService>();
 builder.Services.AddScoped<DeltagerApiService>();
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var apiBaseUrl = builder.Configuration["ApiBaseUrl"] ?? builder.HostEnvironment.BaseAddress;
+if (!apiBaseUrl.EndsWith("/"))
+    apiBaseUrl += "/";
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseUrl) });
 builder.Services.AddScoped<AktivitetApiService>();
 builder.Services.AddScoped<TeamchatAPIService>();
 
 await builder.Build().RunAsync();
-
-var apiBaseUrl = builder.Configuration["ApiBaseUrl"] ?? builder.HostEnvironment.BaseAddress;
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseUrl) });
